748ec1348cadf90170e9597b631c02199d55dd88
scau_zhangpeng <scau_zhangpeng@163.com>
2017-11-21 12:14:00 +0800

    fixBug 1.解决：toolbar没有铺满问题 2.解决：状态栏透明遮罩问题 3.待解决：向上滑动依旧会出现状态栏透明层

--- app/src/main/java/com/dukelight/testbeautifului/ScrollingActivity.java
+++ app/src/main/java/com/dukelight/testbeautifului/ScrollingActivity.java
@@ -3,11 +3,8 @@ package com.dukelight.testbeautifului;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
@@ -57,9 +54,9 @@ public class ScrollingActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
 }
